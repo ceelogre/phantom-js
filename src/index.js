e0834e3e3035d627262fc6dd6c256f0fcb02b8fb
ceelogre <sam.nishimwe@yahoo.com>
2020-09-14 16:30:31 +0200

    Ch(setup) configure linting
    Ch(setup) Configure Swagger

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import express from 'express'
 
 const app = express()
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         message: 'App configured properly.'
     })
 })
 
-const PORT = 3000
+const userIds = [{
+    id: 30
+}]
+app.get('/users', (req, res) => {
+    res.json({
+        userIds
+    })
+})
+let PORT = 3000
+
 app.listen(PORT, () => console.info('Running'))
