de036cfc6a3b3755ba6c3045a915a0828613c936
ceelogre <sam.nishimwe@yahoo.com>
2020-09-24 22:03:57 +0200

    ch(postgres) re-write key/values properly
    ch(postgres) remove production from sequelize config
    
    ch(postgres) set ENV before running tests
    
    ch(postgres) Add proper db URIs and referencing
    
    ch(postgres) view heroku logs
    
    ch(postgres) remove export default from config
    
    ch(postgres) add config to src for babel-build

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,7 @@
 import Sequelize from 'sequelize'
+import config from './config/config'
 
-const db = new Sequelize('postgres://alien@localhost/phantom_development')
+let db = config.env === 'development' ? new Sequelize(process.env.DB_DEV): config.env === 'test' ? new Sequelize(process.env.DB_TEST): new Sequelize(process.env.DATABASE_URL)
 db.authenticate()
   .then(()=>console.log('connection to db successful'))
   .catch(err=>console.log('Error while connecting to db',err))
