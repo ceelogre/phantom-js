d55a384f5dc6f0958c4efc774009ab33aab87cdc
ceelogre <sam.nishimwe@yahoo.com>
2020-09-24 18:36:30 +0200

    ch(lang): Add support for three languages
    ch(lang) fix heroku path resolution
    
    ch(lang) Revert "ch(lang) fix heroku path resolution"
    
    This reverts commit 1455769c2dc3d6ac696bd3a79f1cee863a17a5dd.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import express from 'express'
 import 'dotenv/config'
 import bodyParser from 'body-parser'
+import { I18n } from 'i18n'
+import path from 'path'
+
 import './connection'
 import userRouter from './routes/user'
 
 
+const i18n = new I18n({
+  locales: ['en', 'rw', 'fr'],
+  directory: path.join(__dirname, 'locales')
+})
+
 const app = express()
 app.use(bodyParser.json())
+app.use(i18n.init)
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'App configured properly.'
+    message: res.__('welcome')
   })
 })
 app.use('/users', userRouter)
 
+app.use(
+  (req, res) => {
+    let message = res.__('404')
+    res.json({ message })
+  }
+)
 let PORT = process.env.PORT
 
 app.listen(PORT, () => console.info(`Running on port ${PORT}`))
